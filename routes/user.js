306509364b08b357502464dcfd5a8a02f0a13fc6
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-04 17:20:37 +0300

    add verifiy route

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/users');
+const auth = require('../middleware/is-Auth');
 
 //? POST: /api/users/signup
 router.post('/signup', userController.signup);
 
 //? GET: /api/users/login
 router.get('/login', userController.login);
+
+//? POST /api/users/verifiy
+router.post('/verifiy', auth, userController.verifiy);
 
 module.exports = router;
