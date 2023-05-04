be4b5dcc29c68e37129a0877c268e6f6a72bfd44
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-04 03:56:26 +0300

    fixing Joi schema and add confirm_password property

--- models/user.js
+++ models/user.js
@@ -32,17 +32,17 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 //* User Data validation
-const validateUser = (user) => {
+const validateUser = user => {
 
-  const schema = {
+  const schema = Joi.object ({
     name: Joi.string().min(5).max(30).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(8).max(255).required()
-  }
-
-  return Joi.validate(user, schema);
+    password: Joi.string().min(8).max(255).required(),
+    confirm_password: Joi.string().min(8).max(255).required(),
+  });
+  return schema.validate(user);
 }
 
 
 exports.User = User;
-exports.validateUser = validateUser;
+exports.validate = validateUser;
