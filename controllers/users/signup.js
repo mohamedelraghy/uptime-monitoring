240cc86fda632543d56c83c444b5e07f2e56de8d
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-04 03:19:02 +0300

    adding create user controller

---
+++ controllers/users/signup.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+
+const { User, validate } = require('../../models/user');
+
+module.exports = async (req, res, next) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).json({ "error": error.details[0].message });
+  }
+  try {
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ "error": "User Already registered" });
+    }
+  
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  
+    if (user.password !== req.body.confirm_password) {
+      return res.status(400).json({ "error": "password doesn't match" });
+    } 
+  
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  
+    await user.save();  
+  } catch (err) {
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
+}
