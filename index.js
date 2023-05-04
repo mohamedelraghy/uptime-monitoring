87b8c4cc09bcb627f2cad42d71bd2a7010472bfb
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-04 03:18:22 +0300

    Error middlerware to catch errors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('./startup/middleware')(app);   //* calling some Middleware
 require('./startup/db')();              //* connection to DB 
 require('./startup/routes')(app);       //* API routes
+require('./startup/errorHandler')(app);      //* Error handler
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
