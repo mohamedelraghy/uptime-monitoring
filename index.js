5016121e07e2ca84330ba1d41064edee62d74763
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-05 19:08:23 +0300

    export app for unit tets

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 require('./startup/middleware')(app);   //* calling some Middleware
 require('./startup/db')();              //* connection to DB 
 require('./startup/routes')(app);       //* API routes
-require('./startup/errorHandler')(app);      //* Error handler
+require('./startup/errorHandler')(app); //* Error handler
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+module.exports = app;
