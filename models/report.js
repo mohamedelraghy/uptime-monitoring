c54c3b7997f1ccfebaf9ef304442a68ff5768077
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-07 05:28:14 +0300

    don't need to validate report property

--- models/report.js
+++ models/report.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
 
 //* Report Schema 
 const reportSchema = new mongoose.Schema({
@@ -18,22 +17,4 @@ const reportSchema = new mongoose.Schema({
 //* Report model 
 const Report = mongoose.model('Report', reportSchema);
 
-//* Report Data validation
-const validateReport = report => {
-
-  const schema = Joi.object ({
-    status: Joi.string().required(),
-    availability: Joi.number().positive().required(),
-    ups: Joi.number().number().positive().required(),
-    outages: Joi.number().number().positive().required(),
-    downtime: Joi.number().number().positive().required(),
-    uptime: Joi.number().number().positive().required(),
-    aveResponseTime: Joi.number().number().positive().required(),
-    history: Joi.array().required(),
-    timestamp: Joi.date().required(),
-  });
-  return schema.validate(report);
-}
-
-exports.Report = Report;
-exports.validate = validateReport;
+module.exports = Report;
