a86ae7a8ed3698ace66b9ca0fbc83cd4d5da1c2e
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-09 01:12:54 +0300

    edit some status code

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -17,14 +17,14 @@ module.exports = async(req, res, next) => {
     const user = await User.findOne({email: email});
     if (!user) {
       const error = new Error('A user with this email could not be found.');
-      error.statusCode = 401;
+      error.statusCode = 404;
       throw error;
     }
   
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
       const error = new Error('Invalid email or password');
-      error.statusCode = 401;
+      error.statusCode = 400;
       throw error;
     }
     
