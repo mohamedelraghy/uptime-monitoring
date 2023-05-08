44502bd01d2c86c93a81201a32af0f37ce53fee0
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-08 15:03:49 +0300

    create maling serveces

--- services/sendGrid.js
+++ services/sendGrid.js
@@ -9,4 +9,28 @@ const transport = nodemailer.createTransport(
   })
 );
 
-module.exports = transport;
+exports.sendVerifyCode = async (email, code) => {
+  await transport.sendMail({
+    to: email,
+    from: "elraghy8+noreplay@gmail.com",
+    subject: "Active You Uptime",
+    html: `
+    <p>You just register on Uptime Monitoring</p>
+    <p>Use this code: ${code} to active your Account</p>       
+    `,
+  });
+  
+}
+
+exports.sendPingStatus = async (checkData) => {
+  await transport.sendMail({
+    to: checkData.userEmail,
+    from: "elraghy8+noreplay@gmail.com",
+    subject: `Your ${checkData.name} status`,
+    html: `
+    <p>This is a notification from Uptime Monitoring</p>
+    <p>Your check: ${checkData.protocol}://${checkData.url}:${checkData.port}${checkData.path} status is: ${checkData.status}</p>
+    <p>we Will inform You about any updates</p>      
+    `,
+  });
+}
