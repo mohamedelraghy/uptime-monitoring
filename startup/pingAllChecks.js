78cd55b764ac9deb7cd2b25217d118f787f0857a
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-10 03:28:31 +0300

    fix no check error

--- startup/pingAllChecks.js
+++ startup/pingAllChecks.js
@@ -6,14 +6,18 @@ let ping;
 
 module.exports = async () => {
   console.log('Monitoring....');
-  const checks = await Check.find();
-
-  if(checks.length === 0) {
-    throw new Error('There is no checks to monitor..');
+  try{
+    const checks = await Check.find();
+    if(checks.length === 0) {
+      console.log('There is no check to monitor, please create a check');
+      // throw new Error('There is no checks to monitor..');
+    } else {
+      checks.forEach(async(check) => {
+        ping = await monitor(check);
+        ping.start();
+      });
+    }
+  } catch (error) {
+    throw error;
   }
-
-  checks.forEach(async(check) => {
-    ping = await monitor(check);
-    ping.start();
-  });
 }
