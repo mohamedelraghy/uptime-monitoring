95b2a80539ed640ee7a2eb9b9edc8e48e0a591e9
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-12 04:10:35 +0300

    export server and connect to testDB

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 
 require('./startup/middleware')(app);   //* calling some Middleware
-if (process.env.ENV !== 'test'){
-  require('./startup/db')();              //* connection to DB 
-}
+require('./startup/db')();              //* connection to DB 
 require('./startup/routes')(app);       //* API routes
 require('./startup/errorHandler')(app); //* Error handler
 require('./startup/pingAllChecks')();   //* Start Ping all checks exists in DB;
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
 
-module.exports = app;
+module.exports = server;
