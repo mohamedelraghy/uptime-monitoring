ec8b8de8be29a7817b0c3ec71e79f8d20db55324
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-13 03:41:40 +0300

    rename checkId to id

--- routes/report.js
+++ routes/report.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const reportController = require('../controllers/report');
 const auth = require('../middleware/is-Auth');
+const validID = require('../middleware/validateObjectId');
 
 //? GET: /api/report/checkId ===> return report for Check
-router.get('/:checkId', auth, reportController.getReport);
+router.get('/:id', auth, validID, reportController.getReport);
 
 module.exports = router;
